2b3986a980cef537109ab74f28508c3ee82bc0fb
Zenway <zenway15@gmail.com>
2025-08-28 19:26:27 -0500

    feat: se añaden modelos para la hu-02 estados,solicitud

---
+++ domain/model/src/main/java/co/com/zenway/model/estado/gateways/EstadoRepository.java
@@ -0,0 +1,4 @@
+package co.com.zenway.model.estado.gateways;
+
+public interface EstadoRepository {
+}
---
+++ domain/model/src/main/java/co/com/zenway/model/solicitud/gateways/SolicitudRepository.java
@@ -0,0 +1,4 @@
+package co.com.zenway.model.solicitud.gateways;
+
+public interface SolicitudRepository {
+}
