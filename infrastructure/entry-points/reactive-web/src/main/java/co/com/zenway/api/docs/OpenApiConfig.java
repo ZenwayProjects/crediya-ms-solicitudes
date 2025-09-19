5909eff86d4ee7be223b2dda8d5517c801eacd6f
Zenway <zenway15@gmail.com>
2025-09-18 20:35:35 -0500

    feature: Se añade bearer a la documentacion

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/zenway/api/docs/OpenApiConfig.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/zenway/api/docs/OpenApiConfig.java
@@ -2,9 +2,12 @@ package co.com.zenway.api.docs;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -22,8 +25,9 @@ import org.springframework.context.annotation.Configuration;
                         name = "Apache 2.0",
                         url = "https://www.apache.org/licenses/LICENSE-2.0"
                 )
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
 )
-
+@SecurityScheme(name = "bearerAuth", scheme = "bearer", type = SecuritySchemeType.HTTP, description = "JWT Bearer authentication", bearerFormat = "JWT")
 public class OpenApiConfig {
 }
